950c395ca46be005c67c84cc06f6507a863fe211
Kamindu <kaminduN@users.noreply.github.com>
2022-07-18 23:27:32 +0530

    adding prebuilt app for demo purpose
    adding as docs folder as this is GH convention to auto publish the site

---
+++ docs/precache-manifest.8693c9c1f2493408f03fa96b44a59c9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fede87cc15d0f5ece45c",
+    "url": "/static/js/main.fede87cc.chunk.js"
+  },
+  {
+    "revision": "41f54fb648771d0c6774",
+    "url": "/static/js/1.41f54fb6.chunk.js"
+  },
+  {
+    "revision": "fede87cc15d0f5ece45c",
+    "url": "/static/css/main.a8557afc.chunk.css"
+  },
+  {
+    "revision": "e3ec1fc7509f61d64787d63f2c1abfc7",
+    "url": "/index.html"
+  }
+];
